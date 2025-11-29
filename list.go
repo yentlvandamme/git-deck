be124717e29ab0b4f7a83bca1d267c6614532bd6
yentlvandamme <yentl.van.damme@gmail.com>
2025-11-29 18:31:50 -0500

    Separate list handlers

--- list.go
+++ list.go
@@ -8,13 +8,22 @@ import (
 )
 
 type ListView struct {
-	App  *tview.Application
-	List *tview.List
+	App      *tview.Application
+	List     *tview.List
+	Branches map[string]Branch
+	Worktree *git.Worktree
 }
 
 func NewListView(app *tview.Application, branches map[string]Branch, worktree *git.Worktree) *ListView {
 	list := tview.NewList()
-	list.ShowSecondaryText(false)
+	listView := ListView{
+		App:      app,
+		List:     list,
+		Branches: branches,
+		Worktree: worktree,
+	}
+
+	listView.List.ShowSecondaryText(false)
 
 	index := 1
 	for _, val := range branches {
@@ -22,49 +31,50 @@ func NewListView(app *tview.Application, branches map[string]Branch, worktree *g
 		index++
 	}
 
-	list.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortCut rune) {
-		selectedBranch, exists := branches[mainText]
-		if !exists {
-			return
+	list.SetInputCapture(listView.SetListInputCaptures)
+	list.SetSelectedFunc(listView.SetListSelectedHandler)
+
+	return &listView
+}
+
+func (lv *ListView) SetListInputCaptures(event *tcell.EventKey) *tcell.EventKey {
+	currentIndex := lv.List.GetCurrentItem()
+	totalLength := len(lv.Branches)
+
+	if event.Rune() == 'j' {
+		if currentIndex < totalLength {
+			lv.List.SetCurrentItem(currentIndex + 1)
 		}
-
-		checkoutOpts := git.CheckoutOptions{
-			Hash:                      plumbing.ZeroHash,
-			Branch:                    selectedBranch.Branch.Name(),
-			Create:                    false,
-			Force:                     false,
-			Keep:                      true,
-			SparseCheckoutDirectories: make([]string, 0),
-		}
-
-		err := worktree.Checkout(&checkoutOpts)
-		if err != nil {
-			printError(err)
-		}
-
-		app.Stop()
-	})
-
-	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		currentIndex := list.GetCurrentItem()
-
-		if event.Rune() == 'j' {
-			if currentIndex < index {
-				list.SetCurrentItem(currentIndex + 1)
-			}
-		}
-
-		if event.Rune() == 'k' {
-			if currentIndex > 0 {
-				list.SetCurrentItem(currentIndex - 1)
-			}
-		}
-
-		return event
-	})
-
-	return &ListView{
-		App:  app,
-		List: list,
 	}
+
+	if event.Rune() == 'k' {
+		if currentIndex > 0 {
+			lv.List.SetCurrentItem(currentIndex - 1)
+		}
+	}
+
+	return event
+}
+
+func (lv *ListView) SetListSelectedHandler(index int, mainText string, secondaryText string, shortCut rune) {
+	selectedBranch, exists := lv.Branches[mainText]
+	if !exists {
+		return
+	}
+
+	checkoutOpts := git.CheckoutOptions{
+		Hash:                      plumbing.ZeroHash,
+		Branch:                    selectedBranch.Branch.Name(),
+		Create:                    false,
+		Force:                     false,
+		Keep:                      true,
+		SparseCheckoutDirectories: make([]string, 0),
+	}
+
+	err := lv.Worktree.Checkout(&checkoutOpts)
+	if err != nil {
+		printError(err)
+	}
+
+	lv.App.Stop()
 }
